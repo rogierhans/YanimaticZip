83ac135fc17b47ea4435d4aa5dca92c10aff5344
Rogier Wuijts <R.H.Wuijts@uu.nl>
2022-03-24 19:13:22 +0100

    Add project files.

---
+++ YanimaticZip/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO.Compression;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string currentPath = @"C:\Users\4001184\Desktop\Lil_Test_Folder";
+        Console.WriteLine("Naam zipfile aub...");
+        string zipPath = currentPath +@"\"+ Console.ReadLine();
+        using (ZipArchive archive = ZipFile.OpenRead(zipPath))
+        {
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                string subfolder = entry.FullName.Split('.').First().Split('_').Last();
+                Directory.CreateDirectory(currentPath + @"\" + subfolder);
+                string destinationPath = currentPath + @"\" + subfolder + @"\" + entry.FullName;
+                entry.ExtractToFile(destinationPath,true);
+            }
+        }
+    }
+}
